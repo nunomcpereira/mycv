f29004fbf3c2c80d68c4b05b4dd4a46a0522b811
pereinu <nuno.pereira@infineon.com>
2020-07-24 16:04:51 +0100

    new hos

--- ui/resources/webapp/controller/View1.controller.js
+++ ui/resources/webapp/controller/View1.controller.js
@@ -6,7 +6,7 @@ sap.ui.define([
 	return Controller.extend("ui.controller.View1", {
 		onInit: function() {
 			var oModel = new sap.ui.model.json.JSONModel();
-			oModel.loadData("https://hxehost:51020/", "", false);
+			oModel.loadData("/resume", "", false);
 			//sap.ui.getCore().setModel(oModel);
 			this.getView().setModel(oModel);
 		}
