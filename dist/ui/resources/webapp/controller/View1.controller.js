0781f0193013ca5b9779c1767b64e6bcc49c2747
Nuno Pereira <nunocpereira@gmail.com>
2024-01-20 23:00:35 +0000

    test

---
+++ dist/ui/resources/webapp/controller/View1.controller.js
@@ -0,0 +1,18 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("ui.controller.View1", {
+		onInit: function() {
+			var oModel = new sap.ui.model.json.JSONModel();
+			oModel.loadData("/resume", "", false);
+			//sap.ui.getCore().setModel(oModel);
+			this.getView().setModel(oModel);
+		},
+		openUrl: function(url)
+		{
+			sap.m.URLHelper.redirect(url, true);
+		}
+	});
+});
