a167f7fe1beeddd9674b209e887a1dba4f097cac
Nuno Pereira <nunocpereira@gmail.com>
2024-01-20 18:48:57 +0000

    test

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp            = require('gulp'); // Gulp core package
 
 // The watch task will be executed upon each file change
-gulp.task('default', function() {
+gulp.task('build', function() {
 });
